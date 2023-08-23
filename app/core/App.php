fc9acc16e60d32a7c7b84d71f60fc1cf2bb725d2
Yuda <mahendrayudapradana@mail.ugm.ac.id>
2023-08-23 23:23:36 +0700

    test

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,15 @@
 class App {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url']) ) {
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 }
