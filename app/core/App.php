97ea0e6a9d7e3ca07fa2b393d4e49279df8a0cc7
Yuda <mahendrayudapradana@mail.ugm.ac.id>
2023-08-23 23:28:32 +0700

    break

--- app/core/App.php
+++ app/core/App.php
@@ -10,7 +10,9 @@ class App {
     public function parseURL()
     {
         if (isset($_GET['url']) ) {
-            $url = $_GET['url'];
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
